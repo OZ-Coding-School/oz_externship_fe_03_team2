29ba7efcb6d79c2ff44f71e5ed2ca453957a5d7a
mminseop <alstjq903@naver.com>
2025-10-16 17:03:21 +0900

    Feat: Header 컴포넌트 pc UI 구현

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,5 +1,51 @@
+import { Link } from 'react-router'
+import Button from '../common/Button'
+
 function Header() {
-  return <div>헤더</div>
+  return (
+    <header className="h-16 border-b border-gray-200 bg-white">
+      <div className="mx-auto flex h-16 max-w-[1280px] items-center justify-center px-[60px]">
+        {/* 로고 */}
+        <Link to="/" className="flex h-12 items-center justify-center">
+          <span className="bg-primary-500 mr-2 flex h-8 w-8 items-center justify-center rounded-lg font-bold text-white">
+            S
+          </span>
+          <span className="text-primary-600 text-[20px] font-bold">
+            StudyHub
+          </span>
+        </Link>
+
+        <div className="ml-auto flex items-center">
+          {/* 네비 */}
+          <div className="flex gap-[32px] text-base text-gray-700">
+            <Link to="#" className="hover:text-primary-500">
+              강의 목록
+            </Link>
+
+            <Link to="#" className="hover:text-primary-500">
+              스터디 그룹
+            </Link>
+
+            <Link to="#" className="hover:text-primary-500">
+              구인 공고
+            </Link>
+          </div>
+          {/* 로그인 + 회원가입 버튼 */}
+          <div className="ml-8 flex items-center justify-center gap-4">
+            <Link
+              to="/login"
+              className="hover:text-primary-500 text-base text-gray-700"
+            >
+              로그인
+            </Link>
+            <Button variant="primary" size="md">
+              회원가입
+            </Button>
+          </div>
+        </div>
+      </div>
+    </header>
+  )
 }
 
 export default Header
