2ff4de1bfce7f136497c77b859c60af7c8615aee
mminseop <alstjq903@naver.com>
2025-10-20 01:30:51 +0900

    Feat: Axios 인스턴스 완료

---
+++ src/api/client.ts
@@ -0,0 +1,29 @@
+import axios, { type AxiosInstance } from 'axios'
+
+const url = import.meta.env.VITE_API_BASE_URL
+
+// 인스턴스
+export const apiClient: AxiosInstance = axios.create({
+  baseURL: url,
+  withCredentials: true, // httpOnly
+  headers: {
+    'Content-Type': 'application/json',
+  },
+})
+
+// api 호출
+export const api = {
+  get: <T>(url: string) => apiClient.get<T>(url).then((res) => res.data),
+
+  post: <T, D = unknown>(url: string, data?: D) =>
+    apiClient.post<T>(url, data).then((res) => res.data),
+
+  put: <T, D = unknown>(url: string, data: D) =>
+    apiClient.put<T>(url, data).then((res) => res.data),
+
+  patch: <T, D = unknown>(url: string, data?: D) =>
+    apiClient.patch<T>(url, data).then((res) => res.data),
+
+  delete: <T = void>(url: string) =>
+    apiClient.delete<T>(url).then((res) => res.data),
+}
