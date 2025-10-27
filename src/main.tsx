ac3d65fa650f4b2f43967ea1e0412322f1a3bd34
mminseop <alstjq903@naver.com>
2025-10-27 18:35:22 +0900

    Feat: MSW 세팅 및 핸들러 추가

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,17 @@ import App from './App.tsx'
 import { BrowserRouter } from 'react-router'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
+async function enableMocking() {
+  if (!import.meta.env.DEV || import.meta.env.VITE_USE_MSW !== 'true') {
+    return
+  }
+
+  const { worker } = await import('./mocks/browser')
+
+  // Service Worker 시작 (Promise 반환)
+  return worker.start()
+}
+
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
@@ -14,11 +25,12 @@ const queryClient = new QueryClient({
     },
   },
 })
-
-createRoot(document.getElementById('root')!).render(
-  <QueryClientProvider client={queryClient}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </QueryClientProvider>
-)
+enableMocking().then(() => {
+  createRoot(document.getElementById('root')!).render(
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </QueryClientProvider>
+  )
+})
