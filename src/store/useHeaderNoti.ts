8ed12e1d967b8540733ac6ba92a23a6111405e64
hyeonseo1109 <alsgustj2@naver.com>
2025-10-31 18:37:13 +0900

    Feat: 알림목록 전역상태

--- src/store/useHeaderNoti.ts
+++ src/store/useHeaderNoti.ts
@@ -1,3 +1,16 @@
 import { create } from 'zustand'
+import type { NotificationResponse } from '../types/apiInterface/NotiInterface'
 
-export const useHeaderNoti = create(() => ({}))
+interface HeaderNotiType {
+  notReadData: NotificationResponse | null
+  setNotReadData: (data: NotificationResponse | null) => void
+  readData: NotificationResponse | null
+  setReadData: (data: NotificationResponse | null) => void
+}
+
+export const useHeaderNoti = create<HeaderNotiType>((set) => ({
+  notReadData: null,
+  setNotReadData: (notReadData) => set({ notReadData }),
+  readData: null,
+  setReadData: (readData) => set({ readData }),
+}))
