ebd69ce9b377bc721247e694086278f93e85cc4a
seun9yoon <seun9yoon@gmail.com>
2025-10-31 10:25:14 +0900

    Feat: 유저 정보 가져오기 추가

--- src/pages/SocialCallback.tsx
+++ src/pages/SocialCallback.tsx
@@ -3,12 +3,15 @@ import { useToken } from '../store/useTokenStore'
 import { useEffect } from 'react'
 import { showToast } from '../utils/showToast'
 import { useKakaoLogin, useNaverLogin } from '../api/services/Auth'
+import { useUserStore } from '../store/useUserStore'
+import Header from '../components/layout/Header'
 
 function SocialCallback() {
   const navigate = useNavigate()
   const { setAccessToken } = useToken()
   const { mutate: naverLogin } = useNaverLogin()
   const { mutate: kakaoLogin } = useKakaoLogin()
+  const { setUser } = useUserStore()
 
   useEffect(() => {
     const params = new URLSearchParams(window.location.search)
@@ -36,6 +39,7 @@ function SocialCallback() {
         {
           onSuccess: (data) => {
             showToast(`${data.detail}`, 'success', '로그인')
+            setUser(data.data.user)
             setAccessToken(data.data.access_token)
             sessionStorage.clear()
             navigate('/')
@@ -53,6 +57,7 @@ function SocialCallback() {
         {
           onSuccess: (data) => {
             showToast(`${data.detail}`, 'success', '로그인')
+            setUser(data.data.user)
             setAccessToken(data.data.access_token)
             navigate('/')
           },
@@ -64,7 +69,15 @@ function SocialCallback() {
       )
     }
   }, [])
-  return <div>소셜로그인중</div>
+  return (
+    <div className="flex h-screen flex-col bg-gray-50">
+      <Header />
+      <div className="flex flex-1 flex-col items-center justify-center gap-3">
+        <h1 className="text-primary-500 text-5xl">로그인 하는 중...</h1>
+        <p className="text-gray-600">잠시만 기다려주세요</p>
+      </div>
+    </div>
+  )
 }
 
 export default SocialCallback
