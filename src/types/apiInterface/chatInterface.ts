89f104ffcb424cb53a15e7d96c1ae9bf469bfd64
hyeonseo1109 <alsgustj2@naver.com>
2025-11-02 22:03:42 +0900

    Feat: Chat Interface 작성

--- src/types/apiInterface/chatInterface.ts
+++ src/types/apiInterface/chatInterface.ts
@@ -1,3 +1,4 @@
+// Chat_02 채팅방 목록 조회
 export interface ChatType {
   status: string
   code: string
@@ -21,4 +22,22 @@ export interface Chat {
   file_url: null
   is_read: boolean
   created_at: string
+}
+
+// Chat_03 메시지 내역 조회
+export interface ChatSearchParameter {
+  study_group_id: number
+  keyword: string
+  page?: number
+  size?: number
+}
+
+export interface ChatSearchResponse {
+  status: 'success' | 'error'
+  code: 'SUCCESS' | 'NOT_FOUND'
+  message: string
+  data: {
+    messages: Chat[]
+    total_count: number
+  } | null
 }
