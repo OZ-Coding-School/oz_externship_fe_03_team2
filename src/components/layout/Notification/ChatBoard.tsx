b6de265a805134a1b8a1119dc92b61f7ebe39230
hyeonseo1109 <alsgustj2@naver.com>
2025-11-03 04:24:36 +0900

    Feat: 채팅보드

--- src/components/layout/Notification/ChatBoard.tsx
+++ src/components/layout/Notification/ChatBoard.tsx
@@ -1,5 +1,6 @@
 import { X } from 'lucide-react'
-import { useNotificationStore } from '../../../store/useNotificationStore'
+import { chatData } from '../../NotiDummy'
+import { monthDayFormat } from '../../../utils/dateFormat'
 
 interface ChatOpenType {
   setChatOpen: (chatOpen: boolean) => void
@@ -7,24 +8,51 @@ interface ChatOpenType {
 //프롭스로 챗오픈 상태 받아와서 안에서 엑스바튼으로 열었다 닫았다 할 수 있게
 
 export function ChatBoard({ setChatOpen }: ChatOpenType) {
-  const { notiMessage } = useNotificationStore()
-  const notiCount = notiMessage.count
+  // const { data: chatData } = useChatRooms()
+  const chatCount = chatData.data.pagination.total_count
   return (
-    <div className="w-full max-w-[19.875rem] border bg-white shadow">
-      <div className="flex w-full justify-between">
-        <div className="flex flex-col items-start justify-center bg-gray-50 p-1">
-          <p className="font-semibold">채팅방</p>
-          {notiCount > 0 ? (
+    <div className="flex h-96 w-80 flex-col overflow-hidden rounded-lg border border-gray-200 bg-white shadow select-none">
+      <div className="flex w-full justify-between border-b border-gray-200 bg-gray-50 p-4">
+        <div className="flex flex-col items-start justify-center p-1">
+          <p className="text-black">채팅방</p>
+          {chatCount > 0 ? (
             <p className="text-primary-600 text-xs">
-              {notiCount}개의 읽지 않은 메시지
+              {chatCount}개의 읽지 않은 메시지
             </p>
           ) : (
             <p></p>
           )}
         </div>
-        <div className="text-gray-400 hover:text-gray-500 active:text-gray-600">
-          <X size={55} onClick={() => setChatOpen(false)} />
+        <div className="flex items-center text-gray-400 hover:text-gray-500 active:text-gray-600">
+          <X size={20} onClick={() => setChatOpen(false)} />
         </div>
+      </div>
+      <div className="scrollbar-hide overflow-y-scroll">
+        {chatData.data.messages.map((msg) => (
+          <div
+            key={msg.id}
+            className="flex flex-col gap-1 border-b border-gray-200 p-3 hover:bg-gray-50 active:bg-gray-100"
+          >
+            <div className="flex items-start justify-between gap-3">
+              <p className="flex-1 text-sm">{msg.study_group_name}</p>
+              <div className="flex items-center gap-1">
+                <p className="text-xs text-gray-500">
+                  {monthDayFormat(msg.created_at)}
+                </p>
+                {!msg.is_read && (
+                  <div className="bg-danger-500 flex h-5 w-5 items-center justify-center rounded-full text-xs text-white">
+                    1
+                  </div>
+                )}
+              </div>
+            </div>
+            <div className="flex">
+              <p className="mb-2 text-xs text-gray-500">
+                {msg.sender_nickname}: {msg.content}
+              </p>
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   )
--- src/components/layout/Notification/NotiBadge.tsx
+++ src/components/layout/Notification/NotiBadge.tsx
@@ -1,32 +1,30 @@
 import { MessageCircle } from 'lucide-react'
-import { useNotificationStore } from '../../../store/useNotificationStore'
 import { useState } from 'react'
 import { ChatBoard } from './ChatBoard'
-
+import { chatData } from '../../NotiDummy'
 export function NotiBadge() {
-  const { notiMessage } = useNotificationStore()
-  const notiCount = notiMessage.count
   const [chatOpen, setChatOpen] = useState(false)
+  const chatCount = chatData.data.pagination.total_count
+
   return (
-    <div>
+    <div className="fixed right-4 bottom-4 lg:right-5 lg:bottom-5">
       <div
-        onClick={() => {
-          setChatOpen(!chatOpen)
-        }}
-        className="bg-primary-500 fixed right-4 bottom-4 flex h-[3.5rem] w-[3.5rem] items-center justify-center rounded-full text-white shadow-[0_5px_10px_#00000040] lg:right-5 lg:bottom-5 lg:h-[3.7rem] lg:w-[3.7rem]"
+        onClick={() => setChatOpen(!chatOpen)}
+        className="bg-primary-500 flex h-[3.5rem] w-[3.5rem] items-center justify-center rounded-full text-white shadow-[0_5px_10px_#00000040] lg:h-[3.7rem] lg:w-[3.7rem]"
       >
         <MessageCircle size={25} />
-        {notiCount > 0 && (
+        {chatCount > 0 && (
           <div className="bg-danger-500 absolute top-[-6px] right-[-6px] flex h-[1.3rem] w-[1.3rem] items-center justify-center rounded-full text-[.7rem] lg:top-[-7px] lg:right-[-7px] lg:h-[1.4rem] lg:w-[1.4rem] lg:text-[.75rem]">
-            {notiCount}
-          </div>
-        )}
-        {chatOpen && (
-          <div className="absolute bottom-[110%] z-10">
-            <ChatBoard setChatOpen={setChatOpen} />
+            {chatCount}
           </div>
         )}
       </div>
+
+      {chatOpen && (
+        <div className="absolute right-0 bottom-[110%] z-50">
+          <ChatBoard setChatOpen={setChatOpen} />
+        </div>
+      )}
     </div>
   )
 }
