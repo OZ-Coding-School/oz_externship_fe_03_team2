36972299315075e391354c234c8b38be7564081a
hyeonseo1109 <alsgustj2@naver.com>
2025-11-15 02:08:54 +0900

    Feat: 업버튼보다 채팅보드가 위로 가게

--- src/components/UpButton.tsx
+++ src/components/UpButton.tsx
@@ -28,7 +28,7 @@ export function UpButton() {
   return (
     <div
       onClick={handleScrollToTop}
-      className={`fixed z-1000 ${user ? 'right-4 bottom-22 lg:right-5 lg:bottom-25' : 'right-4 bottom-4 lg:right-5 lg:bottom-5'}`}
+      className={`fixed z-10 ${user ? 'right-4 bottom-22 lg:right-5 lg:bottom-25' : 'right-4 bottom-4 lg:right-5 lg:bottom-5'}`}
     >
       <div className="flex h-[3.5rem] w-[3.5rem] items-center justify-center rounded-full bg-gray-300 text-white shadow-[0_5px_10px_#00000040] lg:h-[3.7rem] lg:w-[3.7rem]">
         <ArrowUp strokeWidth={4} size={25} />
