02f3cb81c4f117cdf91814497b498e109c82772a
mminseop <alstjq903@naver.com>
2025-11-16 19:39:51 +0900

    Feat: lazy.ts, 로딩폴백 컴포넌트 추가

---
+++ src/routes/lazy.ts
@@ -0,0 +1,11 @@
+import { lazy } from 'react'
+
+export const LayoutPage = lazy(() => import('../pages/LayoutPage'))
+export const MainPage = lazy(() => import('../pages/MainPage'))
+export const MyPage = lazy(() => import('../pages/MyPage'))
+export const LoginPage = lazy(() => import('../pages/LoginPage'))
+export const SignUpPage = lazy(() => import('../pages/SignUpPage'))
+export const EmailFindPage = lazy(() => import('../pages/EmailFindPage'))
+export const PasswordFindPage = lazy(() => import('../pages/PasswordFindPage'))
+export const SocialCallback = lazy(() => import('../pages/SocialCallback'))
+export const NotFoundPage = lazy(() => import('../pages/NotFoundPage'))
